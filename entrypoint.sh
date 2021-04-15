45829aa470617b417a4b3f05057e116bc7920ccb
Vadim Rutkovsky <vrutkovs@redhat.com>
2021-04-15 11:20:53 +0200

    Remove tmpdir

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,12 @@ set -exuo pipefail
 export COSA_SKIP_OVERLAY=1
 
 # tmpdir for cosa
-tmpsrc=$(mktemp -d)
-cp -a /src "${tmpsrc}"/src
 cd "$(mktemp -d)"
 cosa init https://github.com/coreos/fedora-coreos-config --branch next-devel
 
 # Copy overrides
 cp -rvf /overrides/* ./overrides
 
+# build ostree commit
 cosa fetch --update-lockfile
 cosa build ostree
